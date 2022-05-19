f084cfed338508941b89966396d33934cf5ac6d9
psspmoodle <matt.donnelly@camh.ca>
2022-05-19 13:59:20 -0400

    add lang file

---
+++ lang/en/cop.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * EN lang file
+ *
+ * @package    format_cop
+ * @copyright  Matt Donnelly CAMH, 2022
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['addsections'] = 'Add sections';
+$string['currentsection'] = 'This section';
+$string['editsection'] = 'Edit section';
+$string['editsectionname'] = 'Edit section name';
+$string['deletesection'] = 'Delete section';
+$string['newsectionname'] = 'New name for section {$a}';
+$string['sectionname'] = 'Section';
+$string['pluginname'] = 'Community of Practice';
+$string['section0name'] = 'General';
+$string['page-course-view-topics'] = 'Any course main page in COP format';
+$string['page-course-view-topics-x'] = 'Any course page in COP format';
+$string['hidefromothers'] = 'Hide section';
+$string['showfromothers'] = 'Show section';
+$string['privacy:metadata'] = 'The Community of Practice format plugin does not store any personal data.';
+$string['coursesubtitle'] = 'Course subtitle';
+$string['sectionshortname'] = 'Section short name';
+$string['sectionshortname_help'] = 'Text here will be used in the side panel, section, and breadcrumb navigation.';
+$string['jumptomenu'] = 'Jump to…';
+$string['courselogo'] = 'Course logo URL';
+$string['descriptorlesson'] = 'Lesson: ';
+$string['descriptorsubsection'] = 'Subsection: ';
+$string['progresstext'] = 'Progress';
+$string['mainpage'] = 'Main page';
---
+++ version.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Version details
+ *
+ * @package    format_cop
+ * @copyright  Matt Donnelly CAMH, 2022
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2022051800;
+$plugin->requires  = 2018050800;
+$plugin->component = 'format_cop';
