9f701fb9c4216de5ed4f7acc98d089de9c36599f
psspmoodle <matt.donnelly@camh.ca>
2022-06-28 10:12:07 -0400

    Add Events link

--- lib.php
+++ lib.php
@@ -31,20 +31,20 @@ class format_cop extends format_base {
      */
     public function extend_course_navigation($navigation, navigation_node $node): array
     {
-        global $USER;
+        global $PAGE, $USER;
 //        parent::extend_course_navigation($navigation, $node);
-        $coursenode = $navigation->find($this->courseid, navigation_node::TYPE_COURSE);
-        $coursenode->add(
+        $node->add(
             'Forums summary',
             new moodle_url('/course/format/cop/posts.php', ['id' => $this->courseid]),
             navigation_node::TYPE_CUSTOM,
             'Forums summary',
-            99,
+            'summary',
             new pix_icon('t/viewdetails', 'summary')
         );
+        // Show only forums user can actually access
         $forums = forum_get_readable_forums($USER->id, $this->courseid);
         foreach ($forums as $forum) {
-            $coursenode->add(
+            $node->add(
                 $forum->name,
                 new moodle_url('/mod/forum/view.php', ['id' => $forum->cm->id]),
                 navigation_node::TYPE_ACTIVITY,
@@ -52,6 +52,19 @@ class format_cop extends format_base {
                 $forum->cm->id,
                 new pix_icon('t/unblock', 'forum')
             );
+        }
+        $eventsurl = new moodle_url('/calendar/view.php', ['course' => $this->courseid]);
+        $eventsnode = $node->add(
+            'Events',
+            $eventsurl,
+            navigation_node::TYPE_CUSTOM,
+            'Events',
+            'events',
+            new pix_icon('i/calendar', 'events calendar')
+        );
+        if ($eventsurl->compare($PAGE->url, URL_MATCH_BASE)) {
+            navigation_node::override_active_url($eventsurl);
+            $eventsnode->make_active();
         }
         return [];
     }
